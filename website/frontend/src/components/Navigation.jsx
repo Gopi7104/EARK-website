f9ca065e118de679cf0801588843b81401e13329
gopi7104 <havocgopi1403@gmail.com>
2024-09-07 16:37:33 +0530

    nav

--- website/frontend/src/components/Navigation.jsx
+++ website/frontend/src/components/Navigation.jsx
@@ -1,28 +1,28 @@
 import React ,{useState}from 'react'
 import './Navigation.css'
 import'../index.css'
-
-// import {links} from 'react-router-dom'
+import {RiMenu3Line,RiCloseLine} from 'react-icons/ri';
 import schlogo from '../asset/Picture/schlogo.webp';
-// import schlogo from '../asset/Picture/EARK_Logo.png';
-
-// const NarBar =()=>{
-//     const [toggleMenu,setToggleMenu]
-// }
-
 
 const Navigation = () => {
+  const [toggleMenu,setToggleMenu]=useState(false);
   return (
     <div className='nav_bar_main'>
         <div className="nav_logo_container">
-            <img className='nav_logo' src={schlogo} alt="logo image" />
-           <div className="nav_link_container">
+            <img className='nav_logo' src={schlogo} 
+            alt="logo image" />
+        </div>
+              <div className="nav_link_container">
+                 <p><a href='#' >About Us</a></p>
+                 <p><a href='#' >Curriculum</a></p>
+                 <p><a href='#' >Our faculties</a></p>
+              </div>
 
-        <p><a href='#' >About Us</a></p>
-        <p><a href='#' >Curriculum</a></p>
-        <p><a href='#' >Our faculties</a></p>
-            </div>
-            </div>
+                  <div className="nav_bar-menu">
+
+
+                  </div>
+
     </div>
   )
 }
