9fd8fb26bf674731c4b137981c156779df72732f
Sriram-Kumaresan <sriram.k.2613@gmail.com>
2024-09-11 19:12:02 +0530

    home changes

--- website/frontend/src/Index.js
+++ website/frontend/src/Index.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-import App from './App.js'
-import Navigation from './components/Navigation.jsx';
-ReactDOM.render(<Navigation/>,document.getElementById('root'));
 import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import ReactGA from 'react-ga4';
+import App from './App.js'
+import Navigation from './components/Navigation.jsx';
+ReactDOM.render(<Navigation/>,document.getElementById('root'));
 
 ReactGA.initialize('G-0HLNW2W47S');
 
