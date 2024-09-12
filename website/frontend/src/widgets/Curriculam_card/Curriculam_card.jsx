4183cc00a9193f0313b221fe6e955bf11df517dd
Kanishkamohan2411 <kanishkamnkl@gmail.com>
2024-09-12 14:13:55 +0530

    curi

--- website/frontend/src/widgets/Curriculam_card/Curriculam_card.jsx
+++ website/frontend/src/widgets/Curriculam_card/Curriculam_card.jsx
@@ -3,10 +3,10 @@ import './Curriculam_card.css';
 
 const Curriculam_card = ({ name, image, onKnowMore }) => {
   return (
-    <div className="teacher-card">
-      <img src={image} alt={name} className="teacher-image" />
-      <div className="teacher-info">
-        <div className="teacher-name">{name}</div>
+    <div className="curri-card">
+      <img src={image} alt={name} className="curri-image" />
+      <div className="curri-info">
+        <div className="curri-name">{name}</div>
       </div>
       {/* <div className="teacher-hover-content">
         <button className="know-more-btn" onClick={onKnowMore}>Know More</button>
